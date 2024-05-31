423363dd13ba36238e5c07f91e686e157ab8db16
Ishbosheth <85916959+Ishb0sheth@users.noreply.github.com>
2024-05-31 11:00:19 -0500

    first commit

---
+++ src/client/java/net/ishbosheth/filmic/FilmicClient.java
@@ -0,0 +1,10 @@
+package net.ishbosheth.filmic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FilmicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/ishbosheth/filmic/FilmicDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ishbosheth.filmic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FilmicDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
